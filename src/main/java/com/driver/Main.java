9b0a6f0e20cbf03c2ec767fa0924b1b32be58cc4
Dev <kanuj2151@gmail.com>
2023-08-17 18:50:37 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
 public class Main {
-
+    class Product{
+        public int product(int x, int y){
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        
+        public double product(double x, double y) {
+            return y * x;
+        }
+    }
+    
+    /**
+     *
+     * @param args
+     */
+    public void main(String [] args){
+        Product p = new Product();
+        
+        System.out.println(p.product(5, 4));
+        
+        System.out.println(p.product(5, 4, 6));
+        
+        System.out.println(p.product(5.045, 4.4679));
+        
+        
+    } 
 }
