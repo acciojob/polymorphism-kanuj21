919d1ce20db1974c6a7fe7127a082178d83d6ce6
Dev <kanuj2151@gmail.com>
2023-08-23 08:58:12 +0530

    refresh Product

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,17 @@ package com.driver;
 public class Main {
 
     public static void main(String [] args){
-        Product p = new Product();
-        
-        System.out.println(p.product(5, 4));
-        
-        System.out.println(p.product(5, 4, 6));
 
-//        double x = , y = 4.4679;
-
-
-        System.out.println(p.product(5.045, 2.4565465));
+//        Product p = new Product();
+//
+//        System.out.println(p.product(5, 4));
+//
+//        System.out.println(p.product(5, 4, 6));
+//
+////        double x = , y = 4.4679;
+//
+//
+//        System.out.println(p.product(5.045, 2.4565465));
 
         
     } 
--- src/main/java/com/driver/Product.java
+++ src/main/java/com/driver/Product.java
@@ -1,21 +1,2 @@
-package com.driver;
-
-public class Product {
-    public static int product() {
-        return product(0, 0);
-    }
-
-    public static int product(int x, int y) {
-        return x * y;
-    }
-
-    public static int product(int x, int y, int z) {
-        return x * y * z;
-    }
-
-
-    public static double product(double x, double y) {
-        return y * x;
-    }
-
+package com.driver;public class Product {
 }
