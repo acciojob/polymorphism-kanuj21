97e7f8be04edded95f6dc85e4bb9fc34925ad8ea
Dev <kanuj2151@gmail.com>
2023-08-23 09:38:43 +0530

    Product in main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,17 +4,19 @@ public class Main {
 
     public static void main(String [] args){
 
+        Product p = new Product();
+
 //        Product p = new Product();
 //
-//        System.out.println(p.product(5, 4));
-//
-//        System.out.println(p.product(5, 4, 6));
+        System.out.println(p.product(5, 4));
+
+        System.out.println(p.product(5, 4, 6));
 //
 ////        double x = , y = 4.4679;
 //
 //
-//        System.out.println(p.product(5.045, 2.4565465));
+        System.out.println(p.product(5.045, 2.4565465));
 
-        
+
     } 
 }
