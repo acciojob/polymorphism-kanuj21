d9e24b8cec0b4d009c8d0857b1996f5d58f33ee7
Dev <kanuj2151@gmail.com>
2023-08-23 09:45:31 +0530

    Product in main 1.1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,27 @@
 package com.driver;
 
+
 public class Main {
+
+    static class Product {
+        int res;
+        double resDouble;
+
+        public int product(int x, int y){
+            this.res = x * y;
+            return res;
+        }
+
+        public int product(int x, int y, int z){
+            this.res = x * y * z;
+            return res;
+        }
+
+        public double product(double x, double y){
+            this.resDouble = x * y;
+            return resDouble;
+        }
+    }
 
     public static void main(String [] args){
 
