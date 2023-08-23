c8dc69864110244e20f118ca8c8d517446619c22
Dev <kanuj2151@gmail.com>
2023-08-23 07:52:50 +0530

    4.1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,6 @@
 package com.driver;
 
-
-
 public class Main {
-    static class Product{
-        public int product(int x, int y){
-            return x * y;
-        }
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        public double product(double x, double y) {
-            return y * x;
-        }
-
-    }
-    
 
     public static void main(String [] args){
         Product p = new Product();
